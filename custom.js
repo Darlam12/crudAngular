ef00becdfa94015faa96eb64c95a3887c5eea4cf
Darlam12 <84628867+Darlam12@users.noreply.github.com>
2024-04-21 13:58:52 -0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,33 +1,27 @@
 	
-	
+      angular.module("listaCadastro",['ngMessages']).run(function ($rootScope, $http) {
+        $rootScope.contatos = [];
 
-
-		angular.module("listaCadastro",['ngMessages']).run(function ($rootScope, $http) {
-        
+        $rootScope.listSexo = [
+			{codigo: 1, descricao: "Masculino"},
+			{codigo: 2, descricao: "Feminino"}
+	   ];
     
-       
-       })
+     })
 
 
 		.controller("listaCadastroCtrl", function ($scope,$rootScope, $http, uppercaseFilter) {
 			$scope.app = "Formulario";
-
-			$rootScope.contatos = [
-				/*{nome: "Ana", telefone: "9999-8888", data: new Date()},
-				{nome: "Pedro", telefone: "9999-8877", data: new Date()},
-				{nome: "Maria", telefone: "9999-8866", data: new Date()}*/
-			];
-
-
+			
 			$scope.adicionarContato = function (contato) {
 			
 				contato.data = new Date();
-				$scope.contatos.push(angular.copy(contato));
+				$rootScope.contatos.push(angular.copy(contato));
 				delete $scope.contato;
 				$scope.contatoForm.$setPristine();
 			};
 			$scope.apagarContatos = function (contatos) {
-				$scope.contatos = contatos.filter(function (contato) {
+				$rootScope.contatos = contatos.filter(function (contato) {
 					if (!contato.selecionado) return contato;
 				});
 			};
@@ -41,9 +35,6 @@
 				$scope.criterioDeOrdenacao = campo;
 				$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
 			};
-
-
-      
 
 
 		});
